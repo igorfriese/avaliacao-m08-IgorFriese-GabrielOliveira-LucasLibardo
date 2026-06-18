2081f2cbfc605461860df6f8e8b9e144b4083dcb
igorfriese <igorfriese2000@gmail.com>
2026-06-18 19:30:10 -0300

    adiciona pastas e classes

---
+++ avaliacao-m08/Repositories/RepositorioLivro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avaliacao_m08.Repositories
+{
+    public class RepositorioLivro
+    {
+    }
+}
